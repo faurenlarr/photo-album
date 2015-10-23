70ce04e5b92ee87430e791158b65340b6318921e
Lauren Farr <laurenfarr@Lauren-MacBook-Pro.local>
2015-10-23 18:10:30 -0400

    updates to photo album

--- info.js
+++ info.js
@@ -1,18 +1,19 @@
 var albums = [{
 albumName : "Alaska",
 AlbumTitle : "Alaska 2014",
+coverPhoto : "https://scontent-mia1-1.xx.fbcdn.net/hphotos-xfp1/t31.0-8/10295923_10204335407489550_399271083981038202_o.jpg",
 photos: [{
-      imageUrl : "https://scontent-mia1-1.xx.fbcdn.net/hphotos-xfp1/t31.0-8/10295923_10204335407489550_399271083981038202_o.jpg",
-      photoName: "Boat",
+      imageUrl: "https://scontent-mia1-1.xx.fbcdn.net/hphotos-xfp1/t31.0-8/10295923_10204335407489550_399271083981038202_o.jpg",
+      photoName: "Boat"
 
         }, {
           imageUrl : "https://scontent-mia1-1.xx.fbcdn.net/hphotos-prn2/t31.0-8/10403921_10204335376968787_7021720121227607339_o.jpg",
-          photoName: "Flowers",
+          photoName: "Flowers"
 
 
         }, {
           imageUrl : "https://scontent-mia1-1.xx.fbcdn.net/hphotos-xpf1/t31.0-8/10258336_10204334918877335_6482644888482521183_o.jpg",
-          photoName: "Eric and I",
+          photoName: "Eric and I"
 
         }],
 
@@ -22,18 +23,19 @@ photos: [{
 
 { albumName : "Italy",
       AlbumTitle : "Italy 2014",
+      coverPhoto : "https://scontent-mia1-1.xx.fbcdn.net/hphotos-xfa1/t31.0-8/10344362_10204856076225943_2907977481101991025_o.jpg",
       photos: [{
             imageUrl : "https://scontent-mia1-1.xx.fbcdn.net/hphotos-xfa1/t31.0-8/10344362_10204856076225943_2907977481101991025_o.jpg"  ,
-            photoName: "Eric" ,
+            photoName: "Eric"
 
               }, {
                 imageUrl : "https://scontent-mia1-1.xx.fbcdn.net/hphotos-xtl1/t31.0-8/10560421_10204856088866259_2115344475720870944_o.jpg"  ,
-                photoName: "Villa" ,
+                photoName: "Villa"
 
 
               }, {
                 imageUrl : "https://scontent-mia1-1.xx.fbcdn.net/hphotos-prn2/t31.0-8/10521056_10204857512581851_5701843491237566612_o.jpg"  ,
-                photoName: "bridge" ,
+                photoName: "bridge"
 
               }],
 
@@ -43,18 +45,19 @@ photos: [{
 
 { albumName : "NZ",
 AlbumTitle : "New Zealand",
+coverPhoto : "https://scontent-mia1-1.xx.fbcdn.net/hphotos-xpf1/v/t1.0-9/222505_2043923341951_5868474_n.jpg?oh=271365fef72a2f784cd2f8b76f38bf77&oe=56C79258",
 photos: [{
       imageUrl : "https://scontent-mia1-1.xx.fbcdn.net/hphotos-xpf1/v/t1.0-9/222505_2043923341951_5868474_n.jpg?oh=271365fef72a2f784cd2f8b76f38bf77&oe=56C79258"  ,
-      photoName: "Sunrise" ,
+      photoName: "Sunrise"
 
         }, {
           imageUrl : "https://scontent-mia1-1.xx.fbcdn.net/hphotos-prn2/v/t1.0-9/223177_2041439559858_5768405_n.jpg?oh=12b9b9d102827b837954d0d6b369adc8&oe=56C0E412"  ,
-          photoName: "beer" ,
+          photoName: "beer"
 
 
         }, {
           imageUrl : "https://scontent-mia1-1.xx.fbcdn.net/hphotos-xap1/v/t1.0-9/222944_2041770048120_3196604_n.jpg?oh=ebf557fd417a18d1f41c9e5f877f7b85&oe=568B3662"  ,
-          photoName: "Milford Sound" ,
+          photoName: "Milford Sound"
 
         }],
 
@@ -64,18 +67,19 @@ photos: [{
 
   { albumName : "More NZ",
         AlbumTitle : "New Zealand Pt.2",
+        coverPhoto : "https://scontent-mia1-1.xx.fbcdn.net/hphotos-xpa1/v/t1.0-9/183829_1919578553409_4530812_n.jpg?oh=7b3a92737d1dc1356aa502a1362849e8&oe=56CD7F5C",
         photos: [{
               imageUrl : "https://scontent-mia1-1.xx.fbcdn.net/hphotos-xpa1/v/t1.0-9/183829_1919578553409_4530812_n.jpg?oh=7b3a92737d1dc1356aa502a1362849e8&oe=56CD7F5C"  ,
-              photoName: "ocean" ,
+              photoName: "ocean"
 
                 }, {
                   imageUrl : "https://scontent-mia1-1.xx.fbcdn.net/hphotos-xap1/v/t1.0-9/183518_1919579513433_5758114_n.jpg?oh=6ea3471901162bc6b609feb10af5062e&oe=56B78D29" ,
-                  photoName: "sailboats" ,
+                  photoName: "sailboats"
 
 
                 }, {
                   imageUrl : "https://scontent-mia1-1.xx.fbcdn.net/hphotos-xaf1/v/t1.0-9/189454_1919581273477_6930964_n.jpg?oh=432693c0c6e67f3db679dea4cd351c87&oe=56CD80DE"  ,
-                  photoName: "ocean view" ,
+                  photoName: "ocean view"
 
                 }],
 
@@ -85,18 +89,19 @@ photos: [{
 
 { albumName : "Family",
   AlbumTitle : "My Family",
+  coverPhoto : "https://scontent-mia1-1.xx.fbcdn.net/hphotos-ash2/t31.0-8/1796883_10203489692467203_2117569576_o.jpg",
   photos: [{
         imageUrl : "https://scontent-mia1-1.xx.fbcdn.net/hphotos-ash2/t31.0-8/1796883_10203489692467203_2117569576_o.jpg"  ,
-        photoName: "Eric and me" ,
+        photoName: "Eric and me"
 
           }, {
             imageUrl : "https://scontent-mia1-1.xx.fbcdn.net/hphotos-xft1/t31.0-8/1502278_10205817910191191_3184139088682696602_o.jpg"  ,
-            photoName: "kids" ,
+            photoName: "kids"
 
 
           }, {
             imageUrl : "https://scontent-mia1-1.xx.fbcdn.net/hphotos-xaf1/t31.0-8/1557365_10203116522898197_77025420_o.jpg"  ,
-            photoName: "Christmas" ,
+            photoName: "Christmas"
 
           }],
 
@@ -105,18 +110,19 @@ photos: [{
         },
 { albumName : "2014",
       AlbumTitle : "2014",
+      coverPhoto : "https://scontent-mia1-1.xx.fbcdn.net/hphotos-prn2/v/t1.0-9/10377634_10204131572073792_8985141782019065734_n.jpg?oh=af5e0a9db8a31dfacd6bc37f04f71e5a&oe=56864EED",
       photos: [{
             imageUrl : "https://scontent-mia1-1.xx.fbcdn.net/hphotos-prn2/v/t1.0-9/10377634_10204131572073792_8985141782019065734_n.jpg?oh=af5e0a9db8a31dfacd6bc37f04f71e5a&oe=56864EED"  ,
-            photoName: "Girls" ,
+            photoName: "Girls"
 
               }, {
                 imageUrl : "https://scontent-mia1-1.xx.fbcdn.net/hphotos-prn2/v/t1.0-9/10556318_10204610736212596_2238331942348748989_n.jpg?oh=9cd318a6e8f71fbc86049d309b76f06c&oe=56C7A73F"  ,
-                photoName: "CatBomb" ,
+                photoName: "CatBomb"
 
 
               }, {
                 imageUrl : "https://scontent-mia1-1.xx.fbcdn.net/hphotos-prn2/t31.0-8/10499409_10204610741092718_1538486726226970859_o.jpg"  ,
-                photoName: "Gals" ,
+                photoName: "Gals"
 
               }],
 
--- main.js
+++ main.js
@@ -2,9 +2,112 @@
 
 $(document).ready(function(){
 
-  var picTemplate = _.template($('#picTmpl').html());
-   console.log("bitter template: ", bitterTemplate);
-    window.bitterHTML = "";
+
+  // $('.column1').click(function(event){
+  //   event.preventDefault();
+  //   $('.column1').css('display', 'block');
+  //   $('.column2').css('display', 'none');
+  //   $('.column3').css('display','none');
+  //   $('.column1r2').css('display', 'none');
+  //   $('.column2r2').css('display', 'none');
+  //   $('.column3r2').css('display','none');});
+
+// diff views
+// var showPhoto = function () {
+//   $(".album").addClass("hidden");
+//   $(".photoDetails").removeClass("hidden");
+//   $(".photoDetails").append(photoText);
+//   $("aside").addClass("hidden");
+// };
+
+// clickable albums
+
+    $('.column1').click(function(event){
+      event.preventDefault();
+       $('.allpicsview').removeClass("hidden");
+       $('.allcolumns').addClass("hidden");
+       $('.navbar').text("Alaska");
+       $('.navbar').addClass('allpicsstyle');
+       $(".column1").siblings().css("display","none");});
+
+   $('.column2').click(function(event){
+     event.preventDefault();
+     $('.allpicsview').removeClass("hidden");
+     $('.allcolumns').addClass("hidden");
+     $('.navbar').text("Italy");
+     $('.navbar').addClass('allpicsstyle');
+     $(".column2").siblings().css("display","none");});
+
+      $('.column3').click(function(event){
+        event.preventDefault();
+        $('.allpicsview').removeClass("hidden");
+        $('.allcolumns').addClass("hidden");
+        $('.navbar').text("New Zealand");
+        $('.navbar').addClass('allpicsstyle');
+        $(".column3").siblings().css("display","none");});
+
+         $('.column1r2').click(function(event){
+           event.preventDefault();
+           $('.allpicsview').removeClass("hidden");
+           $('.allcolumns').addClass("hidden");
+           $('.navbar').text("New Zealand Pt.2");
+           $('.navbar').addClass('allpicsstyle');
+           $(".column1r2").siblings().css("display","none");});
+
+
+            $('.column2r2').click(function(event){
+              event.preventDefault();
+              $('.allpicsview').removeClass("hidden");
+              $('.allcolumns').addClass("hidden");
+              $('.navbar').text("Family");
+              $('.navbar').addClass('allpicsstyle');
+              $(".column2r2").siblings().css("display","none");});
+
+
+
+               $('.column3r2').click(function(event){
+                 event.preventDefault();
+                 $('.allpicsview').removeClass("hidden");
+                 $('.allcolumns').addClass("hidden");
+                 $('.navbar').text("2014");
+                 $('.navbar').addClass('allpicsstyle');
+                 $(".column3r2").siblings().css("display","none");});
+
+
+
+var picTemplate = _.template($('#picTmpl').html());
+
+
+picstring = "";
+_.each(albums, function(currVal, idx){
+  console.log(currVal);
+  picstring = picTemplate(currVal);
+  if(idx < 3) {
+    $('.column'+(idx+1)).html(picstring);
+  } else {
+    console.log('.column'+(idx+1 - 3)+"r2");
+    $('.column'+(idx + 1 - 3)+"r2").html(picstring);
+  }
+});
+
+  // albums.forEach(function(el) {
+  //   el.coverPhoto.forEach(function(currVal) {
+  //   var pictext = picTemplate(currVal);
+  //     $('.column1').append(pictext)
+  //
+  //   });
+  //
+  // });
+
+// var coverphoto = "<img src='" + albums.photos + "'>";
+//
+// $('.column1').html(coverphoto);
+
+
+
+
+
+
 
 
 
